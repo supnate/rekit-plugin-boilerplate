abbad1b468d1d946c1a4316236f45f3dbee46aed
Nate Wang <pwang7@ebay.com>
2018-12-30 13:42:10 +0800

    Adding test element.

--- src/features/rekit-react/core/hooks.js
+++ src/features/rekit-react/core/hooks.js
@@ -1,1 +1,8 @@
-module.exports = {};
+const test = require('./test');
+
+module.exports = {
+  afterAddComponent(name, args) {
+    console.log('after add component: ', arguments);
+    test.add('component', name, args);
+  },
+};
